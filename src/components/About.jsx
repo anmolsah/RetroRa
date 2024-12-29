3b2b7291eb30e62fa6c41315ee656bf57a5beba7
Anmol Sah <kumarxxx242424@gmail.com>
2024-12-30 01:03:00 +0530

    starded with about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React from "react";
+import useSound from "use-sound";
 
 const About = () => {
+  const [playBeep] = useSound("/sounds/beep.mp3", { volume: 0.5 });
   return (
-    <div>About</div>
-  )
-}
+    <section>
+      <div></div>
+    </section>
+  );
+};
 
-export default About
+export default About;
