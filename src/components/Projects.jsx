1b6bf553730e220c6d4f46f3e4bcf52c4d0b5298
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-01 23:41:50 +0530

    updated projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,22 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const Projects = () => {
+  const categories = ["all", "frontend", "fullstack", "backend"];
   return (
     <section className="py-20 bg-light-primary dark:bg-dark-primary">
-      <div>
+      <div className="container mx-auto px-4 max-w-7xl">
         <motion.h2
-          initial={{ opacity: 0, y: 20 }}
+          initial={{ opacity: 0, y: 50 }}
           whileInView={{ opacity: 1, y: 0 }}
           viewport={{ once: true }}
           className="text-3xl md:text-4xl font-press-start text-center mb-12 text-neon-gold"
         >
           PROJECTS.EXE
         </motion.h2>
+        <motion.div
+        
+        ></motion.div>
       </div>
     </section>
   );
