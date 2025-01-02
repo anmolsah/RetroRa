7c6c3f5d723801ae9e1d517b59c1615b6244b15b
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-03 01:50:50 +0530

    created a file called projects and added projects for the portfolio

---
+++ src/data/projects.js
@@ -0,0 +1,69 @@
+import { title } from "framer-motion/client";
+
+export const projects = [
+  {
+    id: 1,
+    title: "Imagnation",
+    description:
+      "A Text-to-Image Generator SaaS platform built with MERN stack and Razorpay integration. Transform your ideas into visuals!",
+    image: "../assets/imagination.png",
+    techStack: [
+      "React",
+      "Node.js",
+      "MongoDB",
+      "Express.js",
+      "Razorpay",
+      "TailwindCSS",
+    ],
+    demoUrl: "https://imagination-xgvo.vercel.app/",
+    githubUrl: "https://github.com/anmolsah/Imagination",
+    category: "fullstack",
+  },
+  {
+    id: 2,
+    title: "NetflixGemini",
+    description:
+      "A Netflix clone powered by GEMINI API for personalized movie recommendations. Built with React, Redux, and Firebase.",
+    image: "../assets/netflix-hme.png",
+    techStack: [
+      "React",
+      "Redux",
+      "Firebase",
+      "TMDB API",
+      "GEMINI API",
+      "TailwindCSS",
+    ],
+    demoUrl: "https://netflix-gemini.vercel.app/browse",
+    githubUrl: "https://github.com/anmolsah/NetflixGemini",
+    category: "frontend",
+  },
+  {
+    id: 3,
+    title: "Rahi",
+    description:
+      "Personalized AI-powered trip planner! With RAHi, planning your dream vacation has never been easier or more fun.",
+    image: "../assets/rahi1.png",
+    techStack: [
+      "React",
+      "Firebase",
+      "GoogleAuth",
+      "GoogleMapsAPI",
+      "TailwindCSS",
+    ],
+    demoUrl: "https://traveler-chi.vercel.app/",
+    githubUrl: "https://github.com/anmolsah/Traveler",
+    category: "frontend",
+  },
+  {
+    id: 4,
+    title: "Boardy",
+    description:
+      "A simple and collaborative online whiteboard for drawing, sketching, and brainstorming.",
+    image: "../assets/boardyy.jpg",
+    techStack: ["HTML", "CSS", "JavaScript"],
+    demoUrl: "https://anmolsah.github.io/Boardy/whiteboard.html",
+    githubUrl:
+      "https://github.com/anmolsah/anmolsah.github.io/tree/main/Boardy",
+    category: "frontend",
+  },
+];
