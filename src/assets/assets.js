76ed813698b124ad04cc766de162584d43498864
Anmol Sah <kumarxxx242424@gmail.com>
2025-01-11 20:57:05 +0530

    feat: add new images to assets for enhanced visual content

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,13 +1,17 @@
 import image1 from "./image_1.jpg";
 import project1 from "./imagination.png";
-import project2 from './netflix-hme.png';
-import project3 from './rahi1.png';
-import project4 from './boardyy.jpg';
+import project2 from "./netflix-hme.png";
+import project3 from "./rahi1.png";
+import project4 from "./boardyy.jpg";
+import image2 from "./img-2.jpg";
+import image3 from "./my_image.jpg";
 
 export const assets = {
   image1,
   project1,
   project2,
   project3,
-  project4
+  project4,
+  image2,
+  image3,
 };
